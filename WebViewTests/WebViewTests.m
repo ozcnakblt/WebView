4c5efec3ff47c8f6e6f665860b5e04f433a62abd
Özcan Akbulut <ozcan.akbulut@me.com>
2013-11-01 08:09:06 +0200

    Initial commit

---
+++ WebViewTests/WebViewTests.m
@@ -0,0 +1,34 @@
+//
+//  WebViewTests.m
+//  WebViewTests
+//
+//  Created by Özcan Akbulut on 01.11.13.
+//  Copyright (c) 2013 Özcan Akbulut. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebViewTests : XCTestCase
+
+@end
+
+@implementation WebViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
